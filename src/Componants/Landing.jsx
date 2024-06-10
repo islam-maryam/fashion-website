543dc7b137f81fd58e4bdbe31928a27dc8d1feef
maryam islam <islammaryam11@gmail.com>
2024-06-10 21:33:49 +0600

    first commit

---
+++ src/Componants/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Landing = () => {
+    return ( 
+        <div className=' h-screen bg-green-200/40'>
+            <div className='w-[80%] mx-auto'>
+                <h1 className='text-center text-5xl font-bold text-green-500'>
+                    Landing Page
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Landing;
---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Landing from "@/Componants/Landing";
+function Home() {
+  return (
+    <main className="">
+      
+      <Landing />
+      
+    </main>
+  );
+}
+
+
+export default Home;
